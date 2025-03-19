0a4e8bb8799872817e2c6d08b7bdd461994297b2
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-03-19 13:55:34 +0900

    feat: 리졸버 등록

--- src/main/java/com/board/global/config/WebMvcConfig.java
+++ src/main/java/com/board/global/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.board.global.config;
 
 import com.board.global.interceptor.AuthInterceptor;
+import com.board.global.resolver.AuthArgumentResolver;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor interceptor;
+    private final AuthArgumentResolver resolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -16,5 +20,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/articles/**")
                 .addPathPatterns("/api/comments/**")
                 .addPathPatterns("/api/members/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(resolver);
     }
 }
